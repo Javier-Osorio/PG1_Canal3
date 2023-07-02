3f37f541ac8aadc9c9d0359b1cb4d05dbd739fd3
Javier Osorio <josorioe@miumg.edu.gt>
2023-07-02 12:48:17 -0600

    Agregando las clases para Login

--- WebApp/Modelo-Controlador/Dao/LogIn/Dao_Usuarios.cs
+++ WebApp/Modelo-Controlador/Dao/LogIn/Dao_Usuarios.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Web;
 using WebApp.Modelo_Controlador.Connection;
 
-namespace WebApp.Modelo_Controlador.Dao
+namespace WebApp.Modelo_Controlador.Dao.LogIn
 {
-    public class Dao_Login : DataLayer
+    public class Dao_Usuarios : DataLayer
     {
 
     }
